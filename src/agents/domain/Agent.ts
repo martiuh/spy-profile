e07c6fadc0f6b3bf45abfe8b8cefd5dc32ab8f78
Tona González <martiuh@gmail.com>
2023-07-06 10:18:10 -0600

    feat: define agent

---
+++ src/agents/domain/Agent.ts
@@ -0,0 +1,45 @@
+import { faker } from "@faker-js/faker";
+interface AgentResponse {
+  name: {
+    first: string;
+    last: string;
+  };
+  location: {
+    city: string;
+    state: string;
+    country: string;
+  };
+  timezone: {
+    offset: string;
+    description: string;
+  };
+  login: {
+    username: string;
+  };
+  gender: string;
+  dob: {
+    date: string;
+    age: number;
+  };
+  eyeColor: string;
+  picture: {
+    large: string;
+    thumbnail: string;
+  };
+}
+
+interface Agent extends AgentResponse {
+  eyeColor: string;
+}
+
+interface ApiResponse {
+  results: AgentResponse[];
+}
+
+const eyeColors = ["blue", "brown", "green", "hazel", "grey"];
+
+export function mapApiResponseToAgent(response: ApiResponse): Agent {
+  const [agent] = response.results;
+  const eyeColor = faker.helpers.arrayElement(eyeColors);
+  return { ...agent, eyeColor };
+}
