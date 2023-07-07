8cf7f066ebcc6550539db1b97a30b21fdb6d130c
Tona González <martiuh@gmail.com>
2023-07-07 10:22:03 -0600

    refactor: export AGENT_API

--- src/agents/repository/agentRepository.ts
+++ src/agents/repository/agentRepository.ts
@@ -1,8 +1,10 @@
 import { mapApiResponseToAgent } from "../domain/Agent";
 
+export const AGENT_API = "https://randomuser.me/api/";
+
 export const getRandomAgentKey = "get-random-agent";
 export async function getRandomAgent() {
-  const response = await fetch("https://randomuser.me/api/");
+  const response = await fetch(AGENT_API);
 
   if (response.ok) {
     const json = await response.json();
